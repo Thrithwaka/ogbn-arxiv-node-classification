2b32742c10dfe57d4495e1519e9db109fc6dc8e2
Thrithwaka <thrithwakapreethi57@gmail.com>
2026-08-10 14:53:30 +0530

    feat: add GAT as third architecture (bonus: Advanced GNN Architectures), use shared model module

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,3 @@
-from .gnn_models import GCN, GraphSAGE, MODEL_REGISTRY, build_model_from_config
+from .gnn_models import GCN, GraphSAGE, GAT, MODEL_REGISTRY, build_model_from_config
 
-__all__ = ["GCN", "GraphSAGE", "MODEL_REGISTRY", "build_model_from_config"]
+__all__ = ["GCN", "GraphSAGE", "GAT", "MODEL_REGISTRY", "build_model_from_config"]
